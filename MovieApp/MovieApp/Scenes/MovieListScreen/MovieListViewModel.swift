d6632269e603b5b6ffa49f56bb8ac9ec38cc02c6
EmreTanrisever <emretanrisever.34@hotmail.com>
2022-06-29 18:29:28 +0300

    Readme file was updated

--- MovieApp/MovieApp/Scenes/MovieListScreen/MovieListViewModel.swift
+++ MovieApp/MovieApp/Scenes/MovieListScreen/MovieListViewModel.swift
@@ -10,13 +10,20 @@ import Foundation
 struct MovieListViewModel {
     private let networkService = MovieService()
     private let key = "Deneme2"
+    private var isItSaved = false
     
     func getMoviesFromService() async -> [Movie] {
         return await networkService.getData()
     }
 
-    func savedMovieToDB(movie: Movie) {
+    mutating func savedMovieToDB(movie: Movie) {
         var movies = readMovies()
+        for newMovie in movies {
+            if newMovie.original_title == movie.original_title {
+                self.isItSaved = true
+                return
+            }
+        }
         movies.append(movie)
         if let newMovie = try? JSONEncoder().encode(movies) {
             UserDefaults.standard.set(newMovie, forKey: key)
--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # MovieApp
+Movies App Example Project Idea
+
+Four-paged movie list app.
+
+###Keywords
+
+-MVVM
+-Table view
+-Codable
+-SPM
+-SnapKit
+-Kingfisher
+-Cocoapods
+-Network Layer
+-URLSession
+-Singleton
+-UserDefaults
+-Understanding Tab Bar and Navigation Controller Understanding MVVM by separating logic between controller and view model Learning to implement table view Integrate third-party library Build UI programmatically in controller Using UserDefaults Integrate network layer by URLSession Using model(such as Movie) with Codable
+
+Language / Framework
+
+Swift with UIKit Responsive Design
+
+![AppVideo](https://user-images.githubusercontent.com/44719978/176475745-a2020f5e-9564-478f-8d00-9d658c4a71b0.mov)
